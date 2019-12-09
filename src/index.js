5761eba50bc3ddb6af75840ae39924b55e33b386
Israel Efraim de Oliveira <israel.oliveira@edu.univali.br>
2019-12-09 16:20:42 -0300

    Organize repository + add Sentinel 2 Composition algorithm

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import configureStore from "./store";
+import moment from "moment";
+import registerServiceWorker from "./registerServiceWorker";
+import { CLIENT_ID } from "./actions/user";
+import Boot from "./Boot";
+
+const store = configureStore();
+console.log("Store configured");
+window.store = store;
+moment.locale("pt-BR");
+
+let Cassie = () => (
+  <Provider store={store}>
+    <Boot />
+  </Provider>
+);
+
+window.gapi.load("auth2", () => {
+  window.gapi.auth2
+    .init({ client_id: CLIENT_ID })
+    .then(() => {
+      ReactDOM.render(<Cassie />, document.getElementById("root"));
+
+      registerServiceWorker();
+    });
+});
