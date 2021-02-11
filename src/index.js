c13febc99cd4e2ae6ef0a36f3a6ecea43bd81457
Israel Efraim <israel.oliveira@edu.univali.br>
2021-02-10 22:30:06 -0300

    refactor: :construction: Move store logic into store/index.js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import configureStore from "./store";
-import moment from "moment";
-import Boot from "./Boot";
+import React from "react"
+import ReactDOM from "react-dom"
+import { Provider } from "react-redux"
+import configureStore from "./store"
+import moment from "moment"
+import Boot from "./Boot"
 import i18n from './i18n'
 
-const store = configureStore();
-window.store = store;
-window.i18n = i18n;
-moment.locale("pt-BR");
+const store = configureStore()
+window.store = store
+window.i18n = i18n
+moment.locale("pt-BR")
 
 ReactDOM.render(
   <Provider store={store}>
     <Boot />
   </Provider>,
   document.getElementById("root")
-);
+)
