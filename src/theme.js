33e118f80a1a466a21539426d0f88e2a1a896937
Israel Efraim <israel.oliveira@edu.univali.br>
2021-02-15 06:19:14 -0300

    refactor: :art: Remove unnecessary data from mui-theme

--- src/theme.js
+++ src/theme.js
@@ -1,102 +1,57 @@
-import { createMuiTheme } from '@material-ui/core/styles';
-import { teal, pink } from '@material-ui/core/colors';
+import { createMuiTheme } from '@material-ui/core/styles'
+import { teal, pink } from '@material-ui/core/colors'
 
-export const space = (...spaces) => {
-  return spaces.map(s => `${s * 8}px`).join(" ");
-}
-
-const theme = createMuiTheme({});
-
-export default createMuiTheme({
+const theme = createMuiTheme({
   palette: {
     primary: teal,
     secondary: pink,
-  },
-  overrides: {
-    MuiCardContent: {
-      root: {
-        '&:last-child': {
-          paddingBottom: space(2),
-        }
-      },
-    },
-    MuiAppBar: {
-      root: {
-        marginBottom: 0,
-      }
-    },
-    MuiExpansionPanelActions: {
-      root: {
-        padding: space(1),
-      }
-    },
-    MuiExpansionPanelDetails: {
-      root: {
-        padding: space(2, 3),
-      }
-    },
-    MuiPaper: {
-      root: {
-        marginBottom: space(2),
-      }
-    },
-    MuiStepper: {
-      root: {
-        backgroundColor: theme.palette.background.default,
-      }
-    },
-    MuiDialogContent: {
-      root: {
-        padding: space(3),
-      }
-    },
-    MuiTableRow: {
-      root: {
-        height: space(5)
-      },
-    }
   }
-});
+})
 
+// @TODO remove
 export const smallIconButton = outerTheme => ({
   ...outerTheme,
   overrides: {
     MuiIconButton: {
       root: {
-        width: space(4),
-        height: space(4),
+        width: outerTheme.spacing(4),
+        height: outerTheme.spacing(4),
         fontSize: outerTheme.typography.pxToRem(20)
       }
     }
   }
-});
+})
 
+// @TODO remove
 export const mediumIconButton = outerTheme => ({
   ...outerTheme,
   overrides: {
     ...outerTheme.overrides,
     MuiIconButton: {
       root: {
-        width: space(5),
-        height: space(5)
+        width: outerTheme.spacing(5),
+        height: outerTheme.spacing(5)
       }
     }
   }
-});
+})
 
+// @TODO remove
 export const lessPaddedExpansion = outerTheme => ({
   ...outerTheme,
   overrides: {
     ...outerTheme.overrides,
     MuiExpansionPanelSummary: {
       root: {
-        padding: space(0, 2),
+        padding: outerTheme.spacing(0, 2),
       }
     },
     MuiExpansionPanelDetails: {
       root: {
-        padding: space(2, 2),
+        padding: outerTheme.spacing(2, 2),
       }
     },
   }
-});
+})
+
+export default theme
