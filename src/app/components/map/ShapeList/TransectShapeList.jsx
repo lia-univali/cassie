baa0ee50d6cf349304d9acc88d55504d6ba53921
Alisson <ali.steffens@gmail.com>
2021-03-08 16:42:06 -0300

    :art: change shapelist

--- src/app/components/map/ShapeList/TransectShapeList.jsx
+++ src/app/components/map/ShapeList/TransectShapeList.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next'
-
+import { makeStyles } from '@material-ui/core/styles'
 import { Grid, ListItemText } from '@material-ui/core'
 
 import { Remove as RemoveIcon } from '@material-ui/icons'
+import StopIcon from '@material-ui/icons/Stop'
 
 const labels = [
     { class: 'forms.map.transects.stable', color: '#43a047' },
@@ -12,13 +13,20 @@ const labels = [
     { class: 'forms.map.transects.criticallyEroded', color: '#d32f2f' }
 ]
 
+const useStyles = makeStyles(theme => ({
+  icon: {
+    margin: 0,
+    fontSize: '1.75rem',
+  }
+}))
+
 const TransectShapeList = () => {
     const [t] = useTranslation()
-
+    const classes = useStyles()
     return (
         labels.map((label, index) => (
             <Grid key={index} container direction="row">
-                <RemoveIcon style={{ color: label.color }} />
+                <RemoveIcon style={{ color: label.color }} className={classes.icon} />
                 <ListItemText
                     secondary={t(label.class)}
                 />
