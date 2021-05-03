7054d5b4f36181893d4ad35a3825ca5166707dfa
Alisson <ali.steffens@gmail.com>
2021-05-03 13:53:44 -0300

    :tada: add localstorage hook

--- src/app/pages/HomePage/index.jsx
+++ src/app/pages/HomePage/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { push } from "connected-react-router";
 import { useTranslation } from "react-i18next";
@@ -176,6 +176,39 @@ function TransitionLeft(props) {
   return <Slide {...props} direction="left" />;
 }
 
+// Hook
+function useLocalStorage(key, initialValue) {
+  // State to store our value
+  // Pass initial state function to useState so logic is only executed once
+  const [storedValue, setStoredValue] = useState(() => {
+    try {
+      // Get from local storage by key
+      const item = window.localStorage.getItem(key);
+      // Parse stored json or if none return initialValue
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      // If error also return initialValue
+      console.log(error);
+      return initialValue;
+    }
+  });
+  // Return a wrapped version of useState's setter function that ...
+  // ... persists the new value to localStorage.
+  const setValue = (value) => {
+    try {
+      // Allow value to be a function so we have same API as useState
+      const valueToStore =
+        value instanceof Function ? value(storedValue) : value;
+      // Save state
+      setStoredValue(valueToStore);
+      // Save to local storage
+      window.localStorage.setItem(key, JSON.stringify(valueToStore));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return [storedValue, setValue];
+}
 const HomePage = () => {
   const busy = useSelector((state) => state.auth.authenticating);
   const dispatch = useDispatch();
@@ -324,7 +357,7 @@ const HomePage = () => {
   }, []);
 
   const [devAdvOpen, setDevAdvOpen] = React.useState(true);
-  const [cookiesAdvOpen, setCookiesAdvOpen] = React.useState(true);
+  const [cookiesAdvOpen, setCookiesAdvOpen] =  useLocalStorage("showCookiesADV", true);
 
   const handleDevAdvClose = (event, reason) => {
     if (reason === "clickaway") {
